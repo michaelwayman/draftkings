6ab2fbe82ced842308ce087522f365c6a007fa17
Nick Arnold <nickarnold23@gmail.com>
2016-11-18 21:24:34 -0500

    Contest Payouts saving logic in, pre refactor

--- draftkings/basketball/management/commands/contests.py
+++ draftkings/basketball/management/commands/contests.py
@@ -5,12 +5,13 @@ import re
 from os import listdir
 from os.path import isfile, join
 
+from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
 from simplejson.scanner import JSONDecodeError
 from texttable import Texttable
 
-from basketball.constants import CONTEST_BATCH_DIR
-from basketball.models import Player, GameLog, Opponent, OpponentLineup, Contest
+from basketball.constants import CONTEST_BATCH_DIR, CONTEST_PAGE_DIR
+from basketball.models import Player, GameLog, Opponent, OpponentLineup, Contest, ContestPayout
 from basketball.utils.contests import CSVManager as ContestManager
 
 
@@ -62,10 +63,27 @@ class Command(BaseCommand):
                 _apply_contest_information(contest)
 
         if options.get('something'):
-            files = [f for f in listdir(CONTEST_BATCH_DIR) if isfile(join(CONTEST_BATCH_DIR, f))]
-            for file in files:
-                if re.match('(\d{2}-\d{2}-\d{4}\.json)', file):
-                    load_contest_data_by_day(file)
+            contest_batch_files = [f for f in listdir(CONTEST_BATCH_DIR) if isfile(join(CONTEST_BATCH_DIR, f))]
+            for file_name in contest_batch_files:
+                if re.match('(\d{2}-\d{2}-\d{4}\.json)', file_name):
+                    load_contest_data_by_day(f)
+
+            contest_html_files = [f for f in listdir(CONTEST_PAGE_DIR) if isfile(join(CONTEST_PAGE_DIR, f))]
+
+            # Remove invalid files
+            contest_file_exp = re.compile('contest-([0-9]*).html')
+            files = [f for f in contest_html_files if contest_file_exp.match(f)]
+
+            for file_name in files:
+                # pull id from file path
+                dk_id = contest_file_exp.search(file_name).group(1)
+                try:
+                    contest = Contest.objects.get(dk_id=dk_id)
+                    save_contest_payout_data(contest)
+                except Contest.DoesNotExist:
+                    log.warning('No contest with contest dk_id: {dk_id}'.format(
+                        dk_id=dk_id
+                    ))
 
 
 def load_contest_data_by_day(file_name):
@@ -77,8 +95,10 @@ def load_contest_data_by_day(file_name):
                 'Trouble decoding json file {file_name}'.format(
                     file_name=file_name))
             return
+
         contest_dicts = contest_dict['Contests']
         contests = [save_contest_data(contest) for contest in contest_dicts]
+        return contests
 
 
 def save_contest_data(contest_dict):
@@ -99,6 +119,32 @@ def save_contest_data(contest_dict):
                                                         mult_entries_allowed=entries_allowed,
                                                         total_entries=total_entries, prize_pool=prize_pool)
     return contest
+
+
+def save_contest_payout_data(contest):
+
+    with open(join(CONTEST_PAGE_DIR, 'contest-2703870.html')) as file:
+        soup = BeautifulSoup(file, 'html.parser')
+
+    payout_rows = soup.find_all(class_='dk-grid')[-1:][0].contents
+    payout_rows = [_.get_text() for _ in payout_rows if _ != u'\n']
+    multi_range_exp = re.compile('([0-9]+)[strdhn]{2}[\s]-[\s]([0-9]{1,6})[strdhn]{0,2}.*\$([0-9]*.[0-9]*)')
+    single_place_exp = re.compile('([0-9]+)[strdhn]{2}[$]([0-9]*.[0-9]*)')
+
+    for row in payout_rows:
+        row = row.replace('\n', '')
+        if ' - ' in row:
+            matches = multi_range_exp.search(row)
+            start, stop, value = matches.group(1, 2, 3)
+            ContestPayout.objects.create(contest=contest, start=start, stop=stop, value=value)
+        else:
+            matches = single_place_exp.search(row)
+            start, stop, value = matches.group(1, 1, 2)
+            ContestPayout.objects.create(contest=contest, start=start, stop=stop, value=value)
+
+    for span in soup.find_all(class_='entrant-username'):
+        user_name = span['title']
+        Opponent.objects.create(user_name=user_name)
 
 
 def _apply_contest_information(contest):
